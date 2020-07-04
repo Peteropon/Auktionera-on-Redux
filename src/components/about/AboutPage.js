667607fbc954da66aae22a2888cefe9e55e76794
Petros Tsavdaridis <peteropon@gmail.com>
2020-07-04 16:48:17 +0200

    Added home page, about page and a common header.

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const AboutPage = () => (
+  <div>
+    <h3>About</h3>
+    <p>This app uses React, Redux and other cool and helpful libraries.</p>
+  </div>
+);
+
+export default AboutPage;
