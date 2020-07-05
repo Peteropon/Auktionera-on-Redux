f1456e9777b6f31540c81577db1e599e80a6dbff
Petros Tsavdaridis <peteropon@gmail.com>
2020-07-05 18:18:16 +0200

    Created a 404 page.

---
+++ src/components/PageNotFound.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PageNotFound = () => (
+  <h2>
+    Page not found.
+    <br />
+    Or you may have just destroyed internet.
+  </h2>
+);
+
+export default PageNotFound;
