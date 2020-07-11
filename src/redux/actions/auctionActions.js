d49d4fbb4f2d042da7ed61c06d8ff17881b1e989
Petros Tsavdaridis <peteropon@gmail.com>
2020-07-11 12:42:21 +0200

    Created new action for loading auctions with thunk.

--- src/redux/actions/auctionActions.js
+++ src/redux/actions/auctionActions.js
@@ -1,5 +1,23 @@
 import * as types from "./actionTypes";
+import * as courseApi from "../../api/courseApi";
 
 export function createAuction(auction) {
   return { type: types.CREATE_AUCTION, auction };
+}
+
+export function loadAuctionSuccess(auctions) {
+  return { type: types.LOAD_AUCTIONS_SUCCESS, auctions };
+}
+
+export function loadAuctions() {
+  return function (dispatch) {
+    return courseApi
+      .getCourses()
+      .then((auctions) => {
+        dispatch(loadAuctionSuccess(auctions));
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
 }
--- src/redux/reducers/auctionReducer.js
+++ src/redux/reducers/auctionReducer.js
@@ -4,6 +4,8 @@ export default function auctionReducer(state = [], action) {
   switch (action.type) {
     case types.CREATE_AUCTION:
       return [...state, { ...action.auction }];
+    case types.LOAD_AUCTIONS_SUCCESS:
+      return action.auctions;
     default:
       return state;
   }
