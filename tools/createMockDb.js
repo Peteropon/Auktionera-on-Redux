0100d8ab261ccc1590eeb67d5f419e458cccd9df
Petros Tsavdaridis <peteropon@gmail.com>
2020-07-11 22:22:08 +0200

    Modified mock data.

--- tools/createMockDb.js
+++ tools/createMockDb.js
@@ -3,10 +3,10 @@ const fs = require("fs");
 const path = require("path");
 const mockData = require("./mockData");
 
-const { courses, authors } = mockData;
-const data = JSON.stringify({ courses, authors });
+const { auctions, users } = mockData;
+const data = JSON.stringify({ auctions, users });
 const filepath = path.join(__dirname, "db.json");
 
-fs.writeFile(filepath, data, function(err) {
+fs.writeFile(filepath, data, function (err) {
   err ? console.log(err) : console.log("Mock DB created.");
 });
--- tools/mockData.js
+++ tools/mockData.js
@@ -1,92 +1,92 @@
-const courses = [
+const auctions = [
   {
     id: 1,
-    title: "Securing React Apps with Auth0",
+    description: "Securing React Apps with Auth0",
     slug: "react-auth0-authentication-security",
-    authorId: 1,
-    category: "JavaScript"
+    seller: 1,
+    category: "JavaScript",
   },
   {
     id: 2,
-    title: "React: The Big Picture",
+    description: "React: The Big Picture",
     slug: "react-big-picture",
-    authorId: 1,
-    category: "JavaScript"
+    seller: 1,
+    category: "JavaScript",
   },
   {
     id: 3,
-    title: "Creating Reusable React Components",
+    description: "Creating Reusable React Components",
     slug: "react-creating-reusable-components",
-    authorId: 1,
-    category: "JavaScript"
+    seller: 1,
+    category: "JavaScript",
   },
   {
     id: 4,
-    title: "Building a JavaScript Development Environment",
+    description: "Building a JavaScript Development Environment",
     slug: "javascript-development-environment",
-    authorId: 1,
-    category: "JavaScript"
+    seller: 1,
+    category: "JavaScript",
   },
   {
     id: 5,
-    title: "Building Applications with React and Redux",
+    description: "Building Applications with React and Redux",
     slug: "react-redux-react-router-es6",
-    authorId: 1,
-    category: "JavaScript"
+    seller: 1,
+    category: "JavaScript",
   },
   {
     id: 6,
-    title: "Building Applications in React and Flux",
+    description: "Building Applications in React and Flux",
     slug: "react-flux-building-applications",
-    authorId: 1,
-    category: "JavaScript"
+    seller: 1,
+    category: "JavaScript",
   },
   {
     id: 7,
-    title: "Clean Code: Writing Code for Humans",
+    description: "Clean Code: Writing Code for Humans",
     slug: "writing-clean-code-humans",
-    authorId: 1,
-    category: "Software Practices"
+    seller: 1,
+    category: "Software Practices",
   },
   {
     id: 8,
-    title: "Architecting Applications for the Real World",
+    description: "Architecting Applications for the Real World",
     slug: "architecting-applications-dotnet",
-    authorId: 1,
-    category: "Software Architecture"
+    seller: 1,
+    category: "Software Architecture",
   },
   {
     id: 9,
-    title: "Becoming an Outlier: Reprogramming the Developer Mind",
+    description: "Becoming an Outlier: Reprogramming the Developer Mind",
     slug: "career-reboot-for-developer-mind",
-    authorId: 1,
-    category: "Career"
+    seller: 1,
+    category: "Career",
   },
   {
     id: 10,
-    title: "Web Component Fundamentals",
+    description: "Web Component Fundamentals",
     slug: "web-components-shadow-dom",
-    authorId: 1,
-    category: "HTML5"
-  }
+    seller: 1,
+    category: "HTML5",
+  },
 ];
 
-const authors = [
+const users = [
   { id: 1, name: "Cory House" },
   { id: 2, name: "Scott Allen" },
-  { id: 3, name: "Dan Wahlin" }
+  { id: 3, name: "Dan Wahlin" },
 ];
 
 const newCourse = {
   id: null,
-  title: "",
-  authorId: null,
-  category: ""
+  description: "",
+  seller: null,
+  category: "",
 };
 
 // Using CommonJS style export so we can consume via Node (without using Babel-node)
 module.exports = {
   newCourse,
-  courses,
-  authors
+  auctions,
+  users,
 };
