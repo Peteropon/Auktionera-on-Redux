a17db9f4f6d7324f39d7e79a83cd4a1056265e24
Petros Tsavdaridis <peteropon@gmail.com>
2020-07-14 15:28:07 +0200

    Created actions for editing and creating auction.

--- src/redux/actions/auctionActions.js
+++ src/redux/actions/auctionActions.js
@@ -1,12 +1,16 @@
 import * as types from "./actionTypes";
 import * as courseApi from "../../api/courseApi";
 
-export function createAuction(auction) {
-  return { type: types.CREATE_AUCTION, auction };
-}
-
 export function loadAuctionSuccess(auctions) {
   return { type: types.LOAD_AUCTIONS_SUCCESS, auctions };
+}
+
+export function updateAuctionSuccess(auctions) {
+  return { type: types.UPDATE_AUCTION_SUCCESS, auctions };
+}
+
+export function createAuctionSuccess(auctions) {
+  return { type: types.CREATE_AUCTION_SUCCESS, auctions };
 }
 
 export function loadAuctions() {
@@ -15,6 +19,21 @@ export function loadAuctions() {
       .getAuctions()
       .then((auctions) => {
         dispatch(loadAuctionSuccess(auctions));
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
+}
+
+export function saveAuction(auction) {
+  return function (dispatch) {
+    return courseApi
+      .saveAuction(auction)
+      .then((savedAuction) => {
+        auction.id
+          ? dispatch(updateAuctionSuccess(savedAuction))
+          : dispatch(createAuctionSuccess(savedAuction));
       })
       .catch((error) => {
         throw error;
--- src/redux/reducers/auctionReducer.js
+++ src/redux/reducers/auctionReducer.js
@@ -3,10 +3,14 @@ import initialState from "./initialState";
 
 export default function auctionReducer(state = initialState.auctions, action) {
   switch (action.type) {
-    case types.CREATE_AUCTION:
+    case types.CREATE_AUCTION_SUCCESS:
       return [...state, { ...action.auction }];
     case types.LOAD_AUCTIONS_SUCCESS:
       return action.auctions;
+    case types.UPDATE_AUCTION_SUCCESS:
+      return state.map((auction) => {
+        auction.id === action.auction.id ? action.auction : auction;
+      });
     default:
       return state;
   }
