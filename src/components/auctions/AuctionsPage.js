00596684c7582c518a344ed4bf1a1d498f3b9e85
Petros Tsavdaridis <peteropon@gmail.com>
2020-07-15 13:44:28 +0200

    Changed comp from class to function.

--- src/components/auctions/AuctionsPage.js
+++ src/components/auctions/AuctionsPage.js
@@ -1,60 +1,65 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
-import * as auctionActions from "../../redux/actions/auctionActions";
-import * as userActions from "../../redux/actions/userActions";
-import * as categoryActions from "../../redux/actions/categoryActions";
+import { loadAuctions } from "../../redux/actions/auctionActions";
+import { loadUsers } from "../../redux/actions/userActions";
+import { loadCategories } from "../../redux/actions/categoryActions";
 import PropTypes from "prop-types";
-import { bindActionCreators } from "redux";
 import AuctionList from "./AuctionList";
 
-class AuctionsPage extends React.Component {
-  componentDidMount() {
-    const { auctions, users, actions, categories } = this.props;
-    if (auctions.length === 0) {
-      actions.loadAuctions().catch((error) => {
-        alert("Loading auctions failed" + error);
-      });
-    }
-
+function AuctionsPage({
+  loadAuctions,
+  loadCategories,
+  loadUsers,
+  auctions,
+  users,
+  categories,
+}) {
+  useEffect(() => {
     if (users.length === 0) {
-      actions.loadUsers().catch((error) => {
+      loadUsers().catch((error) => {
         alert("Loading users failed" + error);
       });
     }
 
     if (categories.length === 0) {
-      actions.loadCategories().catch((error) => {
+      loadCategories().catch((error) => {
         alert("Loading categories failed" + error);
       });
     }
-  }
 
-  render() {
-    return (
-      <>
-        <h2>Ongoing auctions</h2>
-        <AuctionList auctions={this.props.auctions}></AuctionList>
-      </>
-    );
-  }
+    if (auctions.length === 0) {
+      loadAuctions().catch((error) => {
+        alert("Loading auctions failed" + error);
+      });
+    }
+  }, []);
+
+  return (
+    <>
+      <h2>Ongoing auctions</h2>
+      <AuctionList auctions={auctions} />
+    </>
+  );
 }
 
 AuctionsPage.propTypes = {
   auctions: PropTypes.array.isRequired,
-  actions: PropTypes.object.isRequired,
   users: PropTypes.array.isRequired,
   categories: PropTypes.array.isRequired,
+  loadAuctions: PropTypes.func.isRequired,
+  loadUsers: PropTypes.func.isRequired,
+  loadCategories: PropTypes.func.isRequired,
 };
 
 function mapStateToProps(state) {
   return {
     auctions:
-      state.users.length === 0
+      state.users.length === 0 || state.categories.length === 0
         ? []
         : state.auctions.map((auction) => {
             return {
               ...auction,
-              userName: state.users.find((u) => u.id === auction.seller).name,
+              userName: state.users.find((u) => u.id === auction.user).name,
               category: state.categories.find((c) => c.id === auction.category)
                 .name,
             };
@@ -63,18 +68,13 @@ function mapStateToProps(state) {
     categories: state.categories,
   };
 }
+/*  || 
+, */
 
-function mapDispatchToProps(dispatch) {
-  return {
-    actions: {
-      loadAuctions: bindActionCreators(auctionActions.loadAuctions, dispatch),
-      loadUsers: bindActionCreators(userActions.loadUsers, dispatch),
-      loadCategories: bindActionCreators(
-        categoryActions.loadCategories,
-        dispatch
-      ),
-    },
-  };
-}
+const mapDispatchToProps = {
+  loadAuctions,
+  loadUsers,
+  loadCategories,
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(AuctionsPage);
