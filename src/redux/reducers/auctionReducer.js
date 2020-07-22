92706afb84363206b810d9e77910b1d2c52638fe
Petros Tsavdaridis <peteropon@gmail.com>
2020-07-22 23:26:31 +0200

    Fixed wrong spelling

--- src/redux/reducers/auctionReducer.js
+++ src/redux/reducers/auctionReducer.js
@@ -4,12 +4,12 @@ import initialState from "./initialState";
 export default function auctionReducer(state = initialState.auctions, action) {
   switch (action.type) {
     case types.CREATE_AUCTION_SUCCESS:
-      return [...state, { ...action.auction }];
+      return [...state, { ...action.auctions }];
     case types.LOAD_AUCTIONS_SUCCESS:
       return action.auctions;
     case types.UPDATE_AUCTION_SUCCESS:
       return state.map((auction) => {
-        auction.id === action.auction.id ? action.auction : auction;
+        auction.id === action.auctions.id ? action.auctions : auction;
       });
     default:
       return state;
