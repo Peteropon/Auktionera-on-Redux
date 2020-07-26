8e58326668c2a8cc69e73cd3efe6a3487170d980
Petros Tsavdaridis <peteropon@gmail.com>
2020-07-26 17:27:03 +0200

    Enriched the mock data.

--- tools/apiServer.js
+++ tools/apiServer.js
@@ -44,12 +44,11 @@ server.use((req, res, next) => {
   next();
 });
 
-server.post("/courses/", function (req, res, next) {
-  const error = validateCourse(req.body);
+server.post("/auctions/", function (req, res, next) {
+  const error = validateAuction(req.body);
   if (error) {
     res.status(400).send(error);
   } else {
-    req.body.slug = createSlug(req.body.title); // Generate a slug for new courses.
     next();
   }
 });
@@ -65,17 +64,9 @@ server.listen(port, () => {
 
 // Centralized logic
 
-// Returns a URL friendly slug
-function createSlug(value) {
-  return value
-    .replace(/[^a-z0-9_]+/gi, "-")
-    .replace(/^-|-$/g, "")
-    .toLowerCase();
-}
-
-function validateCourse(course) {
-  if (!course.title) return "Title is required.";
-  if (!course.authorId) return "Author is required.";
-  if (!course.category) return "Category is required.";
+function validateAuction(auction) {
+  if (!auction.title) return "Title is required.";
+  if (!auction.user) return "Author is required.";
+  if (!auction.category) return "Category is required.";
   return "";
 }
