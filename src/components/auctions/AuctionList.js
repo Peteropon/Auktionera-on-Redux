763e6dc6635a67d1380d06d6f9337af845f2ea01
Petros Tsavdaridis <peteropon@gmail.com>
2020-07-26 14:09:30 +0200

    Trying new layout with bootstrap card deck instead of list.

--- src/components/auctions/AuctionList.js
+++ src/components/auctions/AuctionList.js
@@ -1,39 +1,62 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import { Card, CardDeck, Container } from "react-bootstrap";
+import photo from "./PageNotFound.png";
 
 const AuctionList = ({ auctions }) => (
-  <table className="table">
-    <thead>
-      <tr>
-        <th />
-        <th>Description</th>
-        <th>Seller</th>
-        <th>Category</th>
-      </tr>
-    </thead>
-    <tbody>
-      {auctions.map((auction) => {
-        return (
-          <tr key={auction.id}>
-            <td>
-              <a
-                className="btn btn-light"
-                href={"http://pluralsight.com/courses/" + auction.slug}
-              >
-                Watch
-              </a>
-            </td>
-            <td>
-              <Link to={"/auction/" + auction.id}>{auction.description}</Link>
-            </td>
-            <td>{auction.userName}</td>
-            <td>{auction.category}</td>
-          </tr>
-        );
-      })}
-    </tbody>
-  </table>
+  <>
+    <Container>
+      <CardDeck>
+        {auctions.map((auction) => {
+          return (
+            <Card className="col" key={auction.id}>
+              <Card.Img variant="top" src={photo} height={200} />
+              <Card.Body>
+                <Card.Title>{auction.description}</Card.Title>
+                <Card.Text>This content is a little bit longer.</Card.Text>
+                <Link to={"/auction/" + auction.id}>See details</Link>
+              </Card.Body>
+              <Card.Footer>
+                <small className="text-muted">Last updated 3 mins ago</small>
+              </Card.Footer>
+            </Card>
+          );
+        })}
+      </CardDeck>
+    </Container>
+    <table className="table">
+      <thead>
+        <tr>
+          <th />
+          <th>Description</th>
+          <th>Seller</th>
+          <th>Category</th>
+        </tr>
+      </thead>
+      <tbody>
+        {auctions.map((auction) => {
+          return (
+            <tr key={auction.id}>
+              <td>
+                <a
+                  className="btn btn-light"
+                  href={"http://pluralsight.com/courses/" + auction.slug}
+                >
+                  Watch
+                </a>
+              </td>
+              <td>
+                <Link to={"/auction/" + auction.id}>{auction.description}</Link>
+              </td>
+              <td>{auction.userName}</td>
+              <td>{auction.category}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  </>
 );
 
 AuctionList.propTypes = {
