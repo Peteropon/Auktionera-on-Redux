5fc58c4da35eeec3a8f2650396556e5d51924562
Petros Tsavdaridis <peteropon@gmail.com>
2020-08-03 15:35:45 +0200

    Activated login func.

--- src/components/login/LoginPage.js
+++ src/components/login/LoginPage.js
@@ -1,14 +1,22 @@
 import React, { useState } from "react";
 import LoginForm from "./LoginForm";
+import { Auth } from "aws-amplify";
+import { toast } from "react-toastify";
+import PropTypes from "prop-types";
+import Spinner from "../common/Spinner";
 
-function LoginPage() {
+function LoginPage({ history }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [saving, setSaving] = useState(false);
   const [errors, setErrors] = useState({});
 
   function formIsValid() {
-    return email.length > 0 && password.length > 0;
+    const errors = {};
+    if (!email) errors.email = "Email is required";
+    if (!password) errors.password = "What about your password buddy?";
+    setErrors(errors);
+    return Object.keys(errors).length === 0;
   }
 
   function handleChange(event) {
@@ -17,20 +25,42 @@ function LoginPage() {
     else if (name === "password") setPassword(value);
   }
 
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
-    console.info(email, password);
+    if (!formIsValid()) return;
+    setSaving(true);
+    try {
+      await Auth.signIn(email, password);
+      toast.success("You have successfully logged in");
+      history.push("/auctions");
+    } catch (e) {
+      toast.error("Login failed " + e.message);
+      console.info(e.message);
+      setSaving(false);
+    } finally {
+      setSaving(false);
+    }
   }
 
   return (
-    <LoginForm
-      email={email}
-      password={password}
-      onChange={handleChange}
-      onSave={handleSubmit}
-      errors={errors}
-      saving={saving}
-    />
+    <>
+      {saving ? (
+        <Spinner />
+      ) : (
+        <LoginForm
+          email={email}
+          password={password}
+          onChange={handleChange}
+          onSave={handleSubmit}
+          errors={errors}
+          saving={saving}
+        />
+      )}
+    </>
   );
 }
+
+LoginPage.propTypes = {
+  history: PropTypes.object.isRequired,
+};
 export default LoginPage;
