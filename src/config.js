1f9464bde2e482cb66d23ed7d6336e897ea0628e
Petros Tsavdaridis <peteropon@gmail.com>
2020-08-03 14:24:36 +0200

    Installed and configured aws amplify.

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "eu-north-1",
+    BUCKET: "auktionera-uploads",
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://c9figduspi.execute-api.us-east-2.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_uZmaIIRKe",
+    APP_CLIENT_ID: "4f9o2m9e7n8lc6skm5huroos46",
+    IDENTITY_POOL_ID: "us-east-2:47f3bc5a-636f-4bee-ad11-79b9abdf93dc",
+  },
+};
--- src/index.js
+++ src/index.js
@@ -6,8 +6,34 @@ import App from "./components/App";
 import "./index.css";
 import configureStore from "./redux/configureStore";
 import { Provider } from "react-redux";
+import { Amplify } from "aws-amplify";
+import config from "./config";
 
 const store = configureStore();
+
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: true,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID,
+  },
+  Storage: {
+    region: config.s3.REGION,
+    bucket: config.s3.BUCKET,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+  },
+  API: {
+    endpoints: [
+      {
+        name: "auctions",
+        endpoint: config.apiGateway.URL,
+        region: config.apiGateway.REGION,
+      },
+    ],
+  },
+});
 
 render(
   <Provider store={store}>
